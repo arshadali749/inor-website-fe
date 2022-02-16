c07c3d09dac2fc2d8e043dbffbb7a4bc1628d7ac
a.ali <aliarshad.memon749@gmail.com>
2022-02-16 18:28:15 -0100

    implemented the code for carousel still in progress need more work...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import HeaderComponent from './components/HeaderComponent';
+import CarouselComponent from './components/CarouselComponent';
 
 function App() {
   return (
     <div className="App">
-    <HeaderComponent></HeaderComponent>
+      <div className="container-fluid">
+        <HeaderComponent></HeaderComponent>
+        <CarouselComponent></CarouselComponent>
+
+      </div>
     </div>
+
   );
 }
 
