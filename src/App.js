029f862905a4357593d684c8f80f1cef714c82a2
a.ali <aliarshad.memon749@gmail.com>
2022-02-18 17:51:22 -0100

    added sidebar components but still in progress.

--- src/App.js
+++ src/App.js
@@ -11,27 +11,37 @@ import Gallary from './components/Gallary';
 import Upcommings from './components/Upcommings';
 import ContactUs from './components/ContactUs';
 import Appointments from './components/Appointments';
+import Leftsidebar from './components/Leftsidebar';
 function App() {
   return (
     <div className="App">
       <div className="container-fluid">
-        <HeaderComponent></HeaderComponent>
-        <CarouselComponent></CarouselComponent>
-        <Routes>
-          <Route path="/" element={<Home />}>
-          </Route>
-          <Route path="/services" element={<Services />}></Route>
-          <Route path="/mission" element={<Mission />}></Route>
-          <Route path="/downloads" element={<Downloads />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/gallary" element={<Gallary />}></Route>
-          <Route path="/upcommings" element={<Upcommings />}></Route>
-          <Route path="/contact" element={<ContactUs />}></Route>
-          <Route path="/appointments" element={<Appointments />}></Route>
-        </Routes>
 
+        <div className="row">
+          <HeaderComponent></HeaderComponent>
+          <CarouselComponent></CarouselComponent>
+        </div>
+
+        <div className="row">
+          <div className="col col-lg-2 leftsidebardiv "> <Leftsidebar></Leftsidebar></div>
+          <div className="col col-lg-8 ">
+            <Routes>
+              <Route path="/" element={<Home />}>
+              </Route>
+              <Route path="/services" element={<Services />}></Route>
+              <Route path="/mission" element={<Mission />}></Route>
+              <Route path="/downloads" element={<Downloads />}></Route>
+              <Route path="/about" element={<About />}></Route>
+              <Route path="/gallary" element={<Gallary />}></Route>
+              <Route path="/upcommings" element={<Upcommings />}></Route>
+              <Route path="/contact" element={<ContactUs />}></Route>
+              <Route path="/appointments" element={<Appointments />}></Route>
+            </Routes>
+          </div>
+          <div className="col col-lg-2 leftsidebardiv" ><Leftsidebar></Leftsidebar></div>
+        </div>
       </div>
-    </div>
+    </div >
 
   );
 }
---
+++ src/components/Leftsidebar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../css/leftsidebar.css"
+
+export default function Leftsidebar() {
+    return (
+        <div>Leftsidebar</div>
+    )
+}
